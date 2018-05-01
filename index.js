64c9cf41e282208587150305f8e366622e75ccbb
Jason Mitchell <jason.mitchell.w@gmail.com>
2018-04-30 18:04:21 -0700

    Removing deprecation warning around node asset imports

--- index.js
+++ index.js
@@ -3,26 +3,10 @@
 module.exports = {
   name: 'ember-nf-graph',
 
-  options: {
-    nodeAssets: {
-      d3: {
-        vendor: ['d3.js']
-      },
-      rx: {
-        vendor: ['dist/rx.all.js']
-      }
-    }
-  },
-
-  included: function(app) {
+  included(app) {
     this._super.included.apply(this, arguments);
 
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app;
-    }
-
-    app.import('vendor/d3/d3.js');
-    app.import('vendor/rx/dist/rx.all.js');
+    this.import('node_modules/d3/d3.js');
+    this.import('node_modules/rx/dist/rx.all.js');
   }
 };
